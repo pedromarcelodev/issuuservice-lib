d293cf2a9204a47df48b46d3d189cd36a302ed8b
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-09-27 04:39:09 -0300

    Include para a biblioteca de funcionalidades

---
+++ issuu-lib.php
@@ -0,0 +1,6 @@
+<?php
+
+require(dirname(__FILE__) . '/lib/class.issuudocument.php');
+require(dirname(__FILE__) . '/lib/class.issuubookmark.php');
+require(dirname(__FILE__) . '/lib/class.issuufolder.php');
+require(dirname(__FILE__) . '/lib/class.issuudocumentembed.php');
